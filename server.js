9ffde927fda55c78b1562c5288dbb781bc0b896d
Arif Billah <emran.alarif8@gmail.com>
2023-09-22 20:33:39 +0600

    code edited

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http =require('http');
 
 const server=http.createServer((req,res)=>{
     res.writeHead(200,{'content-Type':'text/html'});
-    res.end('<h1>hello guys harshit bansal here!</h1>');
+    res.end('<h1>hello guys Arif Billah here!</h1>');
 });
 
 server.listen(8888);
